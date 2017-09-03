826dc90db1a521db6952e9d7846cad2e64a098e3
David Nachman <david@nachman.org>
2017-09-03 14:54:55 -0400

    initial commit

---
+++ ps-springcloud-configclient/src/main/java/com/logicalenigma/psspringcloud/PsSpringcloudConfigclientApplication.java
@@ -0,0 +1,12 @@
+package com.logicalenigma.psspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsSpringcloudConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsSpringcloudConfigclientApplication.class, args);
+	}
+}
