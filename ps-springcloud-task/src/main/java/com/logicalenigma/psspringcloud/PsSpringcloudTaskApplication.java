187ed39b8b107391ff899974e58a9190fb4e6655
David Nachman <david@nachman.org>
2017-09-04 11:12:23 -0400

    update impl

--- ps-springcloud-task/src/main/java/com/logicalenigma/psspringcloud/PsSpringcloudTaskApplication.java
+++ ps-springcloud-task/src/main/java/com/logicalenigma/psspringcloud/PsSpringcloudTaskApplication.java
@@ -13,20 +13,29 @@ public class PsSpringcloudTaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PsSpringcloudTaskApplication.class, args);
 	}
-	
+
 	@Bean
 	public TollProcessingTask tollProcessingTask() {
 		return new TollProcessingTask();
 	}
-	
+
 	public class TollProcessingTask implements CommandLineRunner {
 		@Override
 		public void run(String... args) throws Exception {
 			// parameters stationid, license plate, timestamp
 			if (null != args) {
 				System.out.println("parameter length is " + args.length);
+				if (args.length > 1) {
+					String stationId = args[1];
+					String licensePlate = args[2];
+					String timestamp = args[3];
+
+					System.out.println("Station ID is " + stationId + ", plate is " + licensePlate + ", timestamp is "
+							+ timestamp);
+
+				}
 			}
-			
+
 		}
 	}
 }
