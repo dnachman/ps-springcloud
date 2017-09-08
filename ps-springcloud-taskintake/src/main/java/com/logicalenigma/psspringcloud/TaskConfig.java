3396219046d446fa714d149534c97d3c30bc8f67
David Nachman <david@nachman.org>
2017-09-07 22:28:35 -0400

    lab for tasks - not working with rabbitmq yet

---
+++ ps-springcloud-taskintake/src/main/java/com/logicalenigma/psspringcloud/TaskConfig.java
@@ -0,0 +1,14 @@
+package com.logicalenigma.psspringcloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TaskConfig {
+	
+	@Bean
+	public TaskProcessor taskProcessor() {
+		return new TaskProcessor();
+	}
+
+}
--- ps-springcloud-tasksink/src/test/java/com/logicalenigma/psspringcloud/PsSpringcloudTasksinkApplicationTests.java
+++ ps-springcloud-tasksink/src/test/java/com/logicalenigma/psspringcloud/PsSpringcloudTasksinkApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PsSpringcloudTaskApplicationTests {
+public class PsSpringcloudTasksinkApplicationTests {
 
 	@Test
 	public void contextLoads() {
