a66cac4ec23e6d532b6dbaeecb0525b59052abd6
David Nachman <david@nachman.org>
2017-09-10 15:36:26 -0400

    init

---
+++ ps-springcloud-secureauthserver/src/main/java/com/logicalenigma/psspringcloud/PsSpringcloudSecureauthserverApplication.java
@@ -0,0 +1,14 @@
+package com.logicalenigma.psspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class PsSpringcloudSecureauthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsSpringcloudSecureauthserverApplication.class, args);
+	}
+}
---
+++ ps-springcloud-secureauthserver/src/test/java/com/logicalenigma/psspringcloud/PsSpringcloudSecureauthserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.logicalenigma.psspringcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsSpringcloudSecureauthserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
